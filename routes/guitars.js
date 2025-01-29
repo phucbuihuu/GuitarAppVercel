2a33b2c55afe9b04e6d215c1d0a042a49bf2eb1e
phucbuihuu <phuc.buihuu@outlook.com>
2025-01-29 21:13:23 +0100

    Delete Guitar API

--- routes/guitars.js
+++ routes/guitars.js
@@ -49,6 +49,11 @@ router.post('/', ensureAuth, jsonParser, async function (req, res, next) {
 	const newGuitar = await guitarService.create(Year, Price, BrandId, ModelId, ColorId);
 	res.status(200).json({ success: true, message: "Guitar added successfully.", newGuitar });
 });
+/* DELETE new Guitar */
+router.delete('/:id', ensureAuth, async function (req, res, next) {
+    await guitarService.deleteGuitar(req.params.id)
+    res.status(200).json({success: true, message: "Guitar deleted successfully."});
+})
 
 module.exports = router;
 
--- services/GuitarService.js
+++ services/GuitarService.js
@@ -32,5 +32,11 @@ class GuitarService {
 		return guitar;
 	}
 
+	async deleteGuitar(guitarId) {
+		return this.Guitar.destroy({
+			where: { id: guitarId},
+		})
+	}
+
 }
 module.exports = GuitarService;
